4c468d5daa35af98cc49f5fbaca76cfa4cfaa78e
mr-alede <mr.alede@gmail.com>
2015-11-28 23:04:55 +0300

    missing files added

---
+++ KendoSPA/src/app/main/main.spotsFilter.service.js
@@ -0,0 +1,29 @@
+﻿(function () {
+	'use strict';
+
+	angular
+	  .module('kendoSpa')
+	  .factory('spotsFilterService', spotsFilter);
+
+	function spotsFilter() {
+		var filter = {
+			allOption: 0,
+			dateRange: {},
+			yearMonth: null,
+
+			gridFilter: {
+				spotName: null,
+				campaign: null,
+				hc: 0,
+				status: 0,
+				categorie: 0,
+				language: 0,
+				format: 0,
+				region: 0,
+				vst: null
+			}
+		};
+
+		return filter;
+	}
+})();
